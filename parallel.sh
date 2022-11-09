275b2ebae695fd80b579cd546ae6bd4463f5800d
hrcarryu <hanrui22@outlook.com>
2022-11-09 19:07:47 +0800

    1

--- parallel.sh
+++ parallel.sh
@@ -1,20 +1,22 @@
 #！/bin/bash
-# ./parallel.sh benchmarks/INST_36x36/ 1200 sudoku_acs 6
-# ./parallel.sh test/ 1200 sudoku_acs 6
+# ./parallel.sh benchmarks/INST_36x36 1200 sudoku_acs 1
+# ./parallel.sh test 1200 sudoku_lsc 1
+benchmarks=$1
 time_t=$2
 solver=$3
-order=$4
-seed=$5
+seed=$4
 
 function parallel() {
-    for folder in $@/*;do 
-        index=`basename $folder`
-        path=results/$solver/result_$order
-        if [ ! -d "$path" ];then
-            mkdir -p $path
-        fi
-        nohup bash run.sh $folder $time_t $solver $seed $ > $path/result_$index.log 2>&1 &
+    for file in $@/*;do 
+        index=`basename $file`
+        nohup bash run.sh $file $time_t $solver $seed $ > results/$solver/$@/result_$index.log 2>&1 &
     done
 }
 
-parallel $1
+for folder in $benchmarks/*;do
+    path=results/$solver/$folder
+    if [ ! -d "$path" ];then
+        mkdir -p $path
+    fi
+    parallel $folder
+done
--- run.sh
+++ run.sh
@@ -8,10 +8,10 @@ for file in $folder/*; do
     echo --------------------------------------------------
     start=$[$(date +%s%N)/1000000]
     if [ "$solver"x = "sudoku_acs"x ]; then
-        ./solvers/sudoku_acs/sudokusolver --alg 0 --file $file --timeout $time_t
+        ./solvers/sudoku_acs/sudoku_acs --alg 0 --file $file --timeout $time_t
     fi
     if [ "$solver"x = "sudoku_lsc"x ]; then
-        ./solvers/sudoku_lsc/sudokusolver $file $seed $time_t
+        timeout $time_t ./solvers/sudoku_lsc/sudoku_lsc $file $seed $time_t
     fi
     end=$[$(date +%s%N)/1000000]
     take=$(( end - start ))
