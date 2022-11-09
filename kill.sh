086f103ca22aca96c2cec9b04c6c3b514297df04
hrcarryu <hanrui22@outlook.com>
2022-11-09 19:41:50 +0800

    script

--- kill.sh
+++ kill.sh
@@ -1,1 +1,1 @@
-ps -ef | grep sudokusolver| awk '{print $2}' | xargs kill -9
+ps -ef | grep $1| awk '{print $2}' | xargs kill -9
--- parallel.sh
+++ parallel.sh
@@ -1,13 +1,17 @@
 #！/bin/bash
-# ./parallel.sh benchmarks/INST_36x36 1200 sudoku_acs 1
-# ./parallel.sh test 1200 sudoku_lsc 1
+# ./parallel.sh benchmarks 1000 sudoku_acs 1
+# ./parallel.sh test 1000 sudoku_lsc 1
 benchmarks=$1
 time_t=$2
 solver=$3
 seed=$4
 
 function parallel() {
-    for file in $@/*;do 
+    for file in $@/*;do
+        if test -f $file;then
+            nohup bash run.sh $@ $time_t $solver $seed $ > results/$solver/$@/result.log 2>&1 &
+            return
+        fi
         index=`basename $file`
         nohup bash run.sh $file $time_t $solver $seed $ > results/$solver/$@/result_$index.log 2>&1 &
     done
