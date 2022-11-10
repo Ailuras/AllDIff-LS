0f134946a56f84dc65edcc3ead58097c51291e5d
hrcarryu <hanrui22@outlook.com>
2022-11-10 21:42:06 +0800

    data

--- parallel.sh
+++ parallel.sh
@@ -7,18 +7,20 @@ solver=$3
 seed=$4
 
 function parallel() {
+    foldername=`basename $@`
     for file in $@/*;do
         if test -f $file;then
-            nohup bash run.sh $@ $time_t $solver $seed $ > results/$solver/$@/result.log 2>&1 &
+            nohup bash run.sh $@ $time_t $solver $seed $ > results/$solver/$foldername/result.log 2>&1 &
             return
         fi
         index=`basename $file`
-        nohup bash run.sh $file $time_t $solver $seed $ > results/$solver/$@/result_$index.log 2>&1 &
+        nohup bash run.sh $file $time_t $solver $seed $ > results/$solver/$foldername/result_$index.log 2>&1 &
     done
 }
 
 for folder in $benchmarks/*;do
-    path=results/$solver/$folder
+    foldername=`basename $folder`
+    path=results/$solver/$foldername
     if [ ! -d "$path" ];then
         mkdir -p $path
     fi
