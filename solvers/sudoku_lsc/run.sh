533d17c376ac445285a3b4832d34d5c51d33b1f8
hrcarryu <hanrui22@outlook.com>
2022-12-14 15:35:18 +0800

    1

--- solvers/sudoku_lsc/run.sh
+++ solvers/sudoku_lsc/run.sh
@@ -1,5 +1,5 @@
 g++ sudoku_lsc_re.cpp -std=c++11 -O3 -o sudoku_lsc_re
-./sudoku_lsc_re inst49x49_50_0.txt 1 50 > re.log
+./sudoku_lsc_re inst49x49_50_0.txt 2 50 > re.log
 
 g++ sudoku_lsc_new.cpp -std=c++11 -O3 -o sudoku_lsc_new
-./sudoku_lsc_new inst49x49_50_0.txt 1 50 > new.log
+./sudoku_lsc_new inst49x49_50_0.txt 2 50 > new.log
