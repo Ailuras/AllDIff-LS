ad9d70cbef5b3283b4eaad0f35ec2feb4bec6323
hrcarryu <hanrui22@outlook.com>
2023-03-06 10:09:37 +0800

    1

--- total.sh
+++ total.sh
@@ -1,18 +1,8 @@
-# CP path
-export PATH=solvers/sudoku_csp/MiniZincIDE-2.6.4-bundle-linux-x86_64/bin:$PATH
-export LD_LIBRARY_PATH=solvers/sudoku_csp/MiniZincIDE-2.6.4-bundle-linux-x86_64/lib:$LD_LIBRARY_PATH
-export QT_PLUGIN_PATH=solvers/sudoku_csp/MiniZincIDE-2.6.4-bundle-linux-x86_64/plugins:$QT_PLUGIN_PATH
-# SAT solver
-pip install z3-solver
-# ACS solver
-cd solvers/sudoku_acs/
-make -f Makefile
-cd ../..
-# LSC solver
-cd solvers/sudoku_lsc/
-g++ sudoku_lsc.cpp -std=c++11 -O3 -o sudoku_lsc
+# LS solver
+cd solvers/nqueens_ls/
+# g++ sudoku_lsc.cpp -std=c++11 -O3 -o sudoku_lsc
 g++ NQueen-LS.cpp -std=c++11 -O3 -o NQueen-LS
-g++ NQueen-gen.cpp -std=c++11 -O3 -o NQueen-gen
+# g++ NQueen-gen.cpp -std=c++11 -O3 -o NQueen-gen
 # g++ sudoku_test.cpp -std=c++11 -O3 -o sudoku_lsc
 cd ../..
 # ./parallel.sh benchmarks_hard 1000 sudoku_acs 1
