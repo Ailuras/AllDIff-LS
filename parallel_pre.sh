6aededf92c0e10b1d4fc652681a9cc80f1f7d370
hrcarryu <hanrui22@outlook.com>
2023-03-10 12:57:52 +0800

    1

--- parallel_pre.sh
+++ parallel_pre.sh
@@ -1,12 +1,12 @@
-# cd solvers/nqueens_ls/
-# g++ NQueen-gen.cpp -std=c++11 -O3 -o NQueen-gen
-# cd ../..
+cd solvers/nqueens_ls/
+g++ NQueen-gen.cpp -std=c++11 -O3 -o NQueen-gen
+cd ../..
 
-# for i in {0..9};do
-#     nohup bash run_pre.sh $[$i * 10] $1 >> out.log 2>&1 &
-# done 
+for i in {0..9};do
+    nohup bash run_pre.sh $[$i * 10] $1 >> out.log 2>&1 &
+done 
 
-python script/gen_completion_inst.py $1
+# python script/gen_completion_inst.py $1
 
 # function parallel() {
 #     for prop_folder in $@/*;do
