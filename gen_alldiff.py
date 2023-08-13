9227e0801c54a95897b1adf528c8e913884e5f2e
hrcarryu <hanrui22@outlook.com>
2023-08-13 22:41:41 +0800

    new benchmarks

--- gen_alldiff.py
+++ gen_alldiff.py
@@ -11,22 +11,25 @@ def write_to_file(filename, data):
         for item in data:
             f.write("%s\n" % item)
 
-def generate_files(output_folder, num_files, num_lines):
+def generate_files(output_folder, num_files, num_lines, number):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-    
-    data = [0 for i in range(num_lines)]
-    for p in range(1, num_files+1):
-        data = generate_data(int(num_lines/num_files * p), data)
-        filename = os.path.join(output_folder, f"data_{p}.txt")
-        write_to_file(filename, data)
-        print(f"已生成数据文件：{filename}")
+    for num in range(1, number+1):
+        number_folder = os.path.join(output_folder, str(num))
+        if not os.path.exists(number_folder):
+            os.makedirs(number_folder)
+        data = [0 for i in range(num_lines)]
+        for p in range(1, num_files+1):
+            data = generate_data(int(num_lines/num_files * p), data)
+            filename = os.path.join(number_folder, f"data_{p:0{2}d}.txt")
+            write_to_file(filename, data)
+    print(f"已生成数据文件：{filename}")
 
 def main():
     output_folder = "proportion"  # 指定文件夹路径
-    num_files = 50
+    num_files = 20
     num_lines = 147
-    generate_files(output_folder, num_files, num_lines)
+    generate_files(output_folder, num_files, num_lines, 30)
 
 if __name__ == "__main__":
     main()
