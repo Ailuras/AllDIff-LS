8f1b350bbe796235d489a1b50818c3d372785adb
hrcarryu <hanrui22@outlook.com>
2023-08-20 13:49:14 +0000

    test

--- run.sh
+++ run.sh
@@ -64,6 +64,9 @@ for file in $folder/*; do
     if [ "$solver"x = "sudoku_cpl_des"x ]; then
         timeout $time_t python solvers/sudoku_cpl/sudoku_prop.py solvers/sudoku_cpl/inst49x49_0_0.txt $file
     fi
+    if [ "$solver"x = "sudoku_rule0"x ]; then
+        ./solvers/sudoku_rule0/sudoku_lsc $file $seed $time_t
+    fi
     end=$[$(date +%s%N)/1000000]
     take=$(( end - start ))
     echo $file : ${take} ms.
